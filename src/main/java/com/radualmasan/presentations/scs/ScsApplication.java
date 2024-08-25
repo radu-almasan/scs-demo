fc01410a191074c72ec0b4fc1d0dc9840f1cebdb
Radu Dan Almasan <9264280+radu-almasan@users.noreply.github.com>
2024-08-25 18:34:02 -0500

    functional components

--- src/main/java/com/radualmasan/presentations/scs/ScsApplication.java
+++ src/main/java/com/radualmasan/presentations/scs/ScsApplication.java
@@ -1,13 +1,45 @@
 package com.radualmasan.presentations.scs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class ScsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScsApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<Integer> randomSource() {
+        var rand = new Random();
+        return rand::nextInt;
+    }
+
+    @Bean
+    public Function<Integer, ObjectNode> processor(ObjectMapper objectMapper) {
+        return input -> {
+            var node = objectMapper.createObjectNode();
+            node.put("value", input);
+            return node;
+        };
+    }
+
+    @Bean
+    public Consumer<ObjectNode> consoleLog() {
+        return node -> log.info("{}", node);
+    }
 
 }
