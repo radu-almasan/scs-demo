5b804ba3a7468007222025b49ae93944a4111e11
Radu Dan Almasan <9264280+radu-almasan@users.noreply.github.com>
2024-08-25 18:34:02 -0500

    init

---
+++ src/test/java/com/radualmasan/presentations/scs/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radualmasan.presentations.scs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
